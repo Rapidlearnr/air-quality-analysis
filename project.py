9380ab6b897111694c20307eee6430fcad63e7c6
Md Shaad Akhtar <shanawzakahtar@gmail.com>
2026-04-15 23:57:18 +0530

    Create project.py and load dataset using pandas

---
+++ project.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.linear_model import LinearRegression
+from scipy.stats import norm
+
+df = pd.read_excel("Census_A1_Clean.xlsx")
